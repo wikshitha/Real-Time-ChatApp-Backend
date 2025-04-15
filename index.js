abb2ac1881331dd26797ffa6cd1a4e971d722c86
Wikshitha Umindu <uminduwikshitha@gmail.com>
2025-04-15 11:20:16 +0530

    database connect ✌️

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
+import bodyParser from "body-parser";
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
 
 const app = express();
 
+app.use(bodyParser.json());
+
+let mongoUrl = process.env.MONGO_URL;
+
+mongoose.connect(mongoUrl)
+
+let connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB database connection established successfully");
+})
+
+
+
 app.listen(3000, () => {
-    console.log("Server running on port 3000")
+    console.log("Server is running on port 3000")
 });
